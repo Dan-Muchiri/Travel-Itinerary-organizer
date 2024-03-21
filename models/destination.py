d0f4a34d6f1d6b86058e3851f63ef3d0a5172916
Dan-Muchiri <danspmunene@gmail.com>
2024-03-21 17:33:29 +0300

    Added destination methods

--- models/destination.py
+++ models/destination.py
@@ -34,4 +34,57 @@ class Destination(Base):
         session.add(new_destination)
         session.commit()
 
+    @classmethod
+    def find_by_id(cls, destination_id):
+        """
+        Find a destination by its ID.
+
+        :param destination_id: The ID of the destination to find.
+        :return: The Destination object if found, None otherwise.
+        """
+        return session.query(cls).filter_by(id=destination_id).first()
+
+    @classmethod
+    def find_by_name(cls, name):
+        """
+        Find a destination by its name (case-insensitive and includes partial matches).
+
+        :param name: The name of the destination to find.
+        :return: A list of Destination objects matching the name criteria.
+        """
+        return session.query(cls).filter(cls.name.ilike(f"%{name}%")).all()
+    
+    @staticmethod
+    def delete_destination(destination_id):
+        """
+        Delete a destination from the database.
+
+        :param destination_id: The ID of the destination to delete.
+        """
+        destination = session.query(Destination).get(destination_id)
+        if destination:
+            session.delete(destination)
+            session.commit()
+
+    @staticmethod
+    def update_destination(destination_id, name=None):
+        """
+        Update an existing destination in the database.
+
+        :param destination_id: The ID of the destination to update.
+        :param name: (Optional) The new name of the destination.
+        """
+        destination = session.query(Destination).get(destination_id)
+        if destination:
+            if name:
+                destination.name = name
+            session.commit()
+
+    def get_accommodations(self):
+        """
+        Retrieve all accommodations associated with this destination.
+
+        :return: A list of Accommodation objects representing all accommodations for this destination.
+        """
+        return self.accommodations
     
