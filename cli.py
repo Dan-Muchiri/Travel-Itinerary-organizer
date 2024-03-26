22e7b6d88c7e38480fc9ac4754ed23dd18e95479
Dan-Muchiri <danspmunene@gmail.com>
2024-03-26 17:26:00 +0300

    Edited CLI

--- cli.py
+++ cli.py
@@ -41,10 +41,15 @@ from helpers import (
 )
 
 
+from colorama import init, Fore, Style
+import emoji
+
+init(autoreset=True)
+
 def main():
     while True:
         menu()
-        choice = input("> ")
+        choice = input(Fore.BLUE + "> " + Style.RESET_ALL)
         if choice == "0":
             exit_program()
         elif choice == "1":
@@ -60,11 +65,11 @@ def main():
             activities_menu()
             handle_activities_menu()
         else:
-            print("Invalid choice")
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
 
 def handle_trips_menu():
     while True:
-        choice = input("Trips Menu > ")
+        choice = input(Fore.BLUE + "Trips Menu > " + Style.RESET_ALL)
         if choice == "0":
             return
         elif choice == "5":
@@ -86,11 +91,11 @@ def handle_trips_menu():
         elif choice == "13":
             delete_trip()
         else:
-            print("Invalid choice")
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
 
 def handle_destinations_menu():
     while True:
-        choice = input("Destinations Menu > ")
+        choice = input(Fore.BLUE + "Destinations Menu > " + Style.RESET_ALL)
         if choice == "0":
             return
         elif choice == "14":
@@ -114,11 +119,11 @@ def handle_destinations_menu():
         elif choice == "23":
             delete_destination()
         else:
-            print("Invalid choice")
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
 
 def handle_accommodations_menu():
     while True:
-        choice = input("Accommodations Menu > ")
+        choice = input(Fore.BLUE + "Accommodations Menu > " + Style.RESET_ALL)
         if choice == "0":
             return
         elif choice == "24":
@@ -140,11 +145,11 @@ def handle_accommodations_menu():
         elif choice == "32":
             delete_accommodation()
         else:
-            print("Invalid choice")
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
 
 def handle_activities_menu():
     while True:
-        choice = input("Activities Menu > ")
+        choice = input(Fore.BLUE + "Activities Menu > " + Style.RESET_ALL)
         if choice == "0":
             return
         elif choice == "33":
@@ -166,85 +171,87 @@ def handle_activities_menu():
         elif choice == "41":
             delete_activity()
         else:
-            print("Invalid choice")
-
-
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
 
 def menu():
-    print("")
-    print("***************Welcome to the travel itinerary organizer!***************")
-    print("***********************Browse your planned trips!***********************")
-    print("*****************Trips have many destinations to visit!*****************")
-    print("*The destinations have many accomodations to stay and activities to do!*")
-    print("")
-    print("********Menu********")
-    print("Please select an option:")
-    print("1: Trips Menu")
-    print("2: Destinations Menu")
-    print("3: Accommodations Menu")
-    print("4: Activities Menu")
-    print("0: Exit program")
-    
+    print(Fore.GREEN + "\n🌍🧳🌴 Welcome to the Travel Itinerary Organizer! 🌴🧳🌍")
+    print(Fore.YELLOW + "----------------------------------------------")
+    print(Fore.CYAN + "📅 Browse Your Planned Trips!")
+    print(Fore.CYAN + "🗺️ Trips Have Many Destinations to Visit!")
+    print(Fore.CYAN + "🏨 The Destinations Have Many Accommodations to Stay and Activities to Do!")
+    print(Fore.YELLOW + "----------------------------------------------")
+    print(Fore.YELLOW + "📋 Menu 📋")
+    print(Fore.MAGENTA + "Please select an option:")
+    print("1: " + emoji.emojize(":airplane:") + " Trips Menu")
+    print("2: " + emoji.emojize(":world_map:") + " Destinations Menu")
+    print("3: " + emoji.emojize(":hotel:") + " Accommodations Menu")
+    print("4: " + emoji.emojize(":performing_arts:") + " Activities Menu")
+    print("0: \U0001F6AB" + Fore.RED + " Exit Program")
+
+
 def trips_menu():
     print("")
-    print("********Trips********")
-    print("Please select an option:")
-    print("5. List all trips")
-    print("6. Find trip by name")
-    print("7. Find trip by id")
+    print(Fore.YELLOW + "🛫🧳📜 Trips Menu")
+    print(Fore.CYAN + "Please select an option:")
+    print("5: List all trips")
+    print("6: Find trip by name")
+    print("7: Find trip by id")
     print("8: Get trip's description")
     print("9: Get trip's duration")
     print("10: Get trip's destinations")
     print("11: Create trip")
     print("12: Update trip")
     print("13: Delete trip")
-    print("0: Return to Main Menu")
+    print("0: \U0001F6AB Return to Main Menu")
+
 
 def destinations_menu():
     print("")
-    print("*****Destinations*****")
-    print("Please select an option:")
+    print(Fore.YELLOW + "🗺️🌴📦 Destinations Menu")
+    print(Fore.CYAN + "Please select an option:")
     print("14: List all destinations")
     print("15: Add new destination")
-    print("16. Find destination by name")
-    print("17. Find destination by id")
+    print("16: Find destination by name")
+    print("17: Find destination by id")
     print("18: Get destination's activities sorted by fun rating")
     print("19: Get destination's all accommodations")
     print("20: Find destination's most expensive accommodation")
     print("21: Find destination's cheapest accommodation")
     print("22: Update destination")
     print("23: Delete destination")
-    print("0: Return to Main Menu")
+    print("0: \U0001F6AB Return to Main Menu")
+
 
 def accommodations_menu():
     print("")
-    print("****Accommodations****")
-    print("Please select an option:")
+    print(Fore.YELLOW + "🏨🛏️ Accommodations Menu")
+    print(Fore.CYAN + "Please select an option:")
     print("24: List all accommodations")
     print("25: Add new accommodation")
-    print("26. Find accommodation by name")
-    print("27. Find accommodation by id")
+    print("26: Find accommodation by name")
+    print("27: Find accommodation by id")
     print("28: Get accommodation's notes")
     print("29: List the top 5 most expensive accommodations")
     print("30: List the top 5 cheapest accommodations")
     print("31: Update accommodation")
     print("32: Delete accommodation")
-    print("0: Return to Main Menu")
+    print("0: \U0001F6AB Return to Main Menu")
 
 def activities_menu():
     print("")
-    print("*****Activities*****")
-    print("Please select an option:")
+    print(Fore.YELLOW + "🎭🎉 Activities Menu")
+    print(Fore.CYAN + "Please select an option:")
     print("33: List all activities")
     print("34: Add new activity")
-    print("35. Find activity by name")
-    print("36. Find activity by id")
+    print("35: Find activity by name")
+    print("36: Find activity by id")
     print("37: Get activity's description")
     print("38: List the top 5 most fun activities")
     print("39: List the top 5 least fun activities")
     print("40: Update activity")
     print("41: Delete activity")
-    print("0: Return to Main Menu")
+    print("0: \U0001F6AB Return to Main Menu")
+
 
 
 if __name__ == "__main__":
